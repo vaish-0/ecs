9b58b45d80ea77b913ad93fa3001a007bcb9b0dd
Prasad Thete <prasad.thete@neosoftmail.com>
2022-03-03 17:09:34 +0530

    new

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -13,4 +13,5 @@ yum install docker -y
 systemctl start docker
 systemctl enable docker
 
-
+aws s3 cp s3://jenkins1010/apache.tar apache.tar
+docker load -i apache.tar
---
+++ scripts/kill_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+docker -v
+
+docker container stop apache
+docker container rm apache
