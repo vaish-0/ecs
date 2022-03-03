544c3d9cab05430025290ad678e72dfe2577b3a1
theteprasad <34797315+theteprasad@users.noreply.github.com>
2022-03-03 18:46:56 +0530

    Update after_script.sh

--- scripts/after_script.sh
+++ scripts/after_script.sh
@@ -2,4 +2,4 @@
 
 sudo -i
 
-docker container run -d --name apache -p 80:8080 987240292938.dkr.ecr.ap-south-1.amazonaws.com/http:latest
+docker container run -d --name apache -p 80:80 987240292938.dkr.ecr.ap-south-1.amazonaws.com/http:latest
